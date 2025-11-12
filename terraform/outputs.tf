4aff48664cc8ccc2c3195895fd3664971b9473bb
Sai Sumanvi Tummala <sumanvi.tummala2004@gmail.com>
2025-11-12 22:01:35 +0530

    .

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,9 @@
 output "instance_public_ip" {
-  description = "Public IP of the instance (or the EIP allocation ID when created via Terraform)"
-  value       = var.elastic_ip_allocation_id != "" ? var.elastic_ip_allocation_id : aws_eip.app_eip[0].public_ip
-  # note: if using provided allocation id we return the id you passed; you can call aws ec2 describe-addresses to map
+  value = aws_eip_association.eip_assoc_provided[0].public_ip
 }
 
 output "instance_public_dns" {
-  description = "Public DNS of the instance"
-  value       = aws_instance.app_instance.public_dns
+  value = aws_instance.app_instance.public_dns
 }
 
 output "ecr_repo_url" {
